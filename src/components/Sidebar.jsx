766d441bd6416b08ebf177d162160aa5289687a7
= <gazdarshariq0@gmail.com>
2025-06-24 16:18:53 +0500

    sidebar work!

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import Logo from "../assets/VibelyLogo.png";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faMasksTheater,
+  faMusic,
+} from "@fortawesome/free-solid-svg-icons";
 function Sidebar() {
   return (
     <div className="h-[100vh] w-[22%] bg-secondary">
-      <div className="main-container flex flex-col items-center justify-center h-full">
-        <img src={Logo} alt="Vibely" className=" h-12" />
-        <div className="normal-links">
-          <ul>
+      <div className="main-container  h-full pt-[5%]">
+        <img src={Logo} alt="Vibely" className=" h-12 mx-auto" />
+        <div className="normal-links text-white">
+          <ul className="styled-ul">
             <li>
-              <Link></Link>
+              <Link>
+                <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff" }} />
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <FontAwesomeIcon
+                  icon={faMasksTheater}
+                  style={{ color: "#ffffff" }}
+                />
+                Genre
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <FontAwesomeIcon icon={faMusic} style={{ color: "#ffffff" }} />
+                Artists
+              </Link>
             </li>
           </ul>
         </div>
